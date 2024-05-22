358f48db1ee1809a20a08fb5beed43f7d4cfe739
Julia Costa <juliacosta232005@gmail.com>
2024-05-22 18:06:50 +0100

    Add logic for intialization of Enemies

--- DungeonCrawler/GameInitializer.cs
+++ DungeonCrawler/GameInitializer.cs
@@ -8,19 +8,58 @@ namespace DungeonCrawler
 {
     public class GameInitializer
     {
-        List<Enemy> enemyList;
-        List<Item> itemList;
-        List<Room> roomList;
-
+        Dictionary<string,Enemy> enemyDict = new Dictionary<string, Enemy>();
+        // List<Item> itemList;
+        // List<Room> roomList;
+        string s;
+        string eName;
+        int eHP;
+        int eAtck;
+        int eDef;
+        bool instanceReady = false;
+        
         public void InitializeEnemies()
         {
-            string s;
             using StreamReader sr = new StreamReader("Enemies.txt");
-            
+
             while ((s = sr.ReadLine()) != "END")
             {
-                Console.WriteLine(s);
+                if (s != "")
+                {
+                    if (s[0] == '#') continue;
+                    else 
+                    {
+                        string[] parameters = s.Split(":");
+
+                        switch(parameters[0])
+                        {
+                            case "Name":
+                                eName = parameters[1];
+                                break;
+                            case "HP":
+                                eHP = int.Parse(parameters[1]);
+                                break;
+                            case "AttackPower":
+                                eAtck = int.Parse(parameters[1]);
+                                break;
+                            case "Defense":
+                                eDef = int.Parse(parameters[1]);
+                                instanceReady = true;
+                                break;
+                        }
+                        if (instanceReady)
+                        {
+                            Console.WriteLine("Enemy Created");
+                            enemyDict.Add(eName,new Enemy(eName,eHP,eAtck,eDef));
+                            instanceReady = false;
+                        }
+                    }
+                }
             }
+            foreach (KeyValuePair<string,Enemy> kvp in enemyDict)
+            {
+                Console.WriteLine($"{kvp.Key} : {kvp.Value.HP}, {kvp.Value.AttackPower}, {kvp.Value.Defense}");
+            } 
         }
 
     }
