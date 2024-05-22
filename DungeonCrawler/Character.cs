9bef521f21776377359ac8dcaa905e10dcbeb3f5
Julia Costa <juliacosta232005@gmail.com>
2024-05-22 15:27:16 +0100

    Add class Character and Player and Enemy subclasses, Add logic to class Character

---
+++ DungeonCrawler/Character.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DungeonCrawler
+{
+    public abstract class Character
+    {
+        public string Name { get; }
+        public int HP { get; private set; }
+        public int AttackPower { get; private set; }
+        public int Defense { get; private set; }
+
+        public Character(string name, int hp, int def, int atk)
+        {
+            Name = name;
+            HP = hp;
+            Defense 0 def;
+            AttackPower = atk;
+        }
+        public void Attack (Character target)
+        {
+            int hitPower = AttackPower - target.Defense;
+            if (hitPower < 0) hitPower = 0;
+            target.HP -= hitPower;
+        }
+    }
+}
