3e02d0bd6c70a0e858e37616e379e575832b1dcc
notCroptu <martinsmariana1972@gmail.com>
2024-05-22 17:32:34 +0100

    Remove CheckStatus method from Player Class, Add to Move, PickUpItem, Heal and Equip methods in Player class, Add properties to Player class

--- DungeonCrawler/Player.cs
+++ DungeonCrawler/Player.cs
@@ -7,30 +7,58 @@ namespace DungeonCrawler
 {
     public class Player : Character
     {
-        Item[] inventory = new Item[5];
-        public Player(string name, int hp, int def, int atk) : base(name,hp,def,atk)
+        public Item[] Inventory { get; private set;} = new Item[5];
+        public Item Weapon { get; private set; }
+        public Item Shield { get; private set; }
+        public Room InRoom{ get; private set; }
+        private int _baseAttack;
+        private int _baseDefense;
+        public Player(string name, int hp, int def, int atk, Room room) : base(name,hp,def,atk)
         {
-            
+            _baseAttack = AttackPower;
+            _baseDefense = DefensePower;
+            InRoom = room;
         }
-        public void MoveTo(string direction)
+        public bool Move(string direction)
         {
-
+            if ( room.accessRooms[direction] != null )
+            {
+                InRoom = room.accessRooms[direction];
+                return true;
+            }
+            return false;
         }
-        public void PickUpItem()
+        public void PickUpItem(Item newItem)
         {
-
+            foreach (Item item in Inventory)
+            {
+                if (item == null) item = newItem;
+            }
         }
-        public void Heal()
+        public void Heal(Item potion)
         {
-
+            hp += potion.buff;
+            foreach (Item item in Inventory)
+            {
+                if (item == potion)
+                {
+                    item = null;
+                    break;
+                }
+            }
         }
         public void Equip()
         {
-
-        }
-        public void CheckStatus()
-        {
-
+            if (newItem is Weapon)
+            {
+                AttackPower = _baseAttack + newItem.AttackPower;
+                Weapon = newItem;
+            }
+            else if (newItem is Shield)
+            {
+                Defense = _baseDefense + newItem.Defense;
+                Shield = newItem;
+            }
         }
     }
 }
