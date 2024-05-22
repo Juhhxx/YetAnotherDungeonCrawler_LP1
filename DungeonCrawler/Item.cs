92e756a79f8125a1f1d939229f2dbd3f5ef7b11f
notCroptu <martinsmariana1972@gmail.com>
2024-05-22 18:07:09 +0100

    Add BuffType property to Item class

--- DungeonCrawler/Item.cs
+++ DungeonCrawler/Item.cs
@@ -8,11 +8,13 @@ namespace DungeonCrawler
     public abstract class Item
     {
         public string Name { get; }
+        public BuffType Type { get; }
         public int Buff { get; }
 
-        public Item(string name, int buff)
+        public Item(string name, BuffType type, int buff)
         {
             Name = name;
+            Type = type;
             Buff = buff;
         }
     }
