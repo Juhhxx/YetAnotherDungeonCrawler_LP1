827df17ee655c32ec2c03a8687621b2a6ed581d7
Julia Costa <juliacosta232005@gmail.com>
2024-05-22 15:39:30 +0100

    Add logic to Player class

--- DungeonCrawler/Player.cs
+++ DungeonCrawler/Player.cs
@@ -7,6 +7,12 @@ namespace DungeonCrawler
 {
     public class Player : Character
     {
-        
+        Item[] inventory = new Item[5];
+        public Player(string name, int hp, int def, int atk) : base(name,hp,def,atk)
+        public void MoveTo(string direction)
+        public void PickUpItem()
+        public void Heal()
+        public void Equip()
+        public void CheckStatus()
     }
 }
