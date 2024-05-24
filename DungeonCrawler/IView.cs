62faa0537cfd4e36477f16d59b09588e387013d2
Afonso Cunha <afonsocunha.dev@gmail.com>
2024-05-24 13:45:36 +0100

    Complete signatures for methods in IView

--- DungeonCrawler/IView.cs
+++ DungeonCrawler/IView.cs
@@ -8,11 +8,12 @@ namespace DungeonCrawler
     public interface IView
     {
         public void RoomDescription(Room room);
-        public void ActionResult();
-        public void PrintStatus();
-        public void PickupItem();
-        public void ItemInformation();
-        public void TurnNarrator();
+        public void AttackResult(Character characterActive, Character characterPassive );
+        public void MoveResult();
+        public void HealResult(Item potion);
+        public void PrintStatus(Character character);
+        public void PickupItem(Item item);
+        public void ItemInformation(Item item);
         public void ByeBye();
         public void GameOver();
         public void GameWin(); 
