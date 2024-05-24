d5159ccacf0cc68a80e1b238bffcd21a1f167177
Afonso Cunha <afonsocunha.dev@gmail.com>
2024-05-24 13:50:34 +0100

    Correct syntax in gameView; Add body to RoomDescripton()

--- DungeonCrawler/GameView.cs
+++ DungeonCrawler/GameView.cs
@@ -7,6 +7,45 @@ namespace DungeonCrawler
 {
     public class GameView : IView
     {
-        
+        public void RoomDescription(Room room)
+        {
+            Console.WriteLine(Room.Description);
+        }
+        public void AttackResult(Character characterActive, Character characterPassive )
+        {
+
+        }
+        public void MoveResult()
+        {
+
+        }
+        public void HealResult(Item potion)
+        {
+
+        }
+        public void PrintStatus(Character character)
+        {
+
+        }
+        public void PickupItem(Item item)
+        {
+
+        }
+        public void ItemInformation(Item item)
+        {
+
+        }
+        public void ByeBye()
+        {
+
+        }
+        public void GameOver()
+        {
+
+        }
+        public void GameWin()
+        {
+
+        }
     }
 }
