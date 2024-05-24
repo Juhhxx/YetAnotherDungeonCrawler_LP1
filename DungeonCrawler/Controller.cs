3a6f613c660f6edad0ea4524e01273aa0339f681
notCroptu <martinsmariana1972@gmail.com>
2024-05-24 20:08:44 +0100

    Add HandleItemAction to Controller class and edit game loop

--- DungeonCrawler/Controller.cs
+++ DungeonCrawler/Controller.cs
@@ -13,34 +13,93 @@ namespace DungeonCrawler
         private IView view;
         private Player player;
 
-
+        public Controller(IView view, Player player)
+        {
+            
+        }
         public void Start()
         {
             // hello message + instructions
+
+            string input;
+            string[] inputArray;
+            bool hasActed;
 
             while (true)
             {
                 view.RoomDescription(player.InRoom);
 
-
-                string input = view.AwaitDecision();
-                string[] inputArray = input.Trim().ToLower().Split(' ');
-
-                switch (input[0])
+                while ( !hasActed )
                 {
-                    case "go":
-                        if ( player.Move(input[1]) ) continue;
-                    case "pickup":
-                    case "attack":
-                    case "exit":
+                    input = view.AwaitDecision();
+                    inputArray = input.Trim().ToLower().Split(' ');
+
+                    if (inputArray[0] == "go")
+                    {
+                        hasActed = player.Move(inputArray[1]);
+                        hasActed ? view.CanMove() : view.CantMove();
+                    }
+                    else if (inputArray[0] == "pick" && inputArray[1] == "up")
+                    {
+                        hasActed = HandleItemAction(inputArray[2], true, player.PickupItem);
+                    }
+                    else if (inputArray[0] == "equip")
+                    {
+                        hasActed = HandleItemAction(inputArray[1], false, player.Equip);
+                    }
+                    else if (inputArray[0] == "attack")
+                    {
+                        player.Attack(player.InRoom.REnemy)
+                    }
+                    else if (inputArray[0] == "view" && inputArray[1] == "status")
+                    {
+                        view.PlayerStatus(player);
+                    }
+                    else if (inputArray[0] == "exit")
+                    {
                         view.ByeBye();
                         break;
-                    default:
-                        view.
-                        continue;
+                    }
+                    else
+                    {
+                        view.WrongCommand();
+                    }
                 }
 
+                hasActed = false;
             }
+        }
+        public HandleItemAction(string input, Action<Item> playerAction, bool isPotion)
+        {
+            bool result = false;
+
+            if ( !player.CheckForItem() )
+            {
+                // print warning - there is no item to pick up
+            }
+            else
+            {
+                try 
+                {
+                    Item newItem = player.ConfirmItem(input, isPotion);
+
+                    if ( newItem != null)
+                    {
+                        playerAction(newItem);
+                        view.PickupItem(newItem);
+                        result = true;
+                    }
+                    else
+                    {
+                        // print warning - item is not shield or sword
+                    }
+                }
+                catch (Exception e)
+                {
+                    //  print warning - need to insert a name
+                }
+            }
+            return result;
         }
     }
 }
