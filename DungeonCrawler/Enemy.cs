722cc7204cdaa8f25edbcdcff6497e0286510882
notCroptu <martinsmariana1972@gmail.com>
2024-05-26 16:05:19 +0100

    Add documentation to Enemy class

--- DungeonCrawler/Enemy.cs
+++ DungeonCrawler/Enemy.cs
@@ -5,8 +5,16 @@ using System.Threading.Tasks;
 
 namespace DungeonCrawler
 {
+    // Enemy class identifies all the non Player class characters
     public class Enemy : Character
     {
+        /// <summary>
+        /// Enemy constructor
+        /// </summary>
+        /// <param name="name">The name to be used by Enemy which is returned to Character base.</param>
+        /// <param name="hp">The HP to be used by Enemy which is returned to Character base.</param>
+        /// <param name="atk">The attack power to be used by Enemy which is returned to Character base.</param>
+        /// <param name="def">The defense to be used by Enemy which is returned to Character base.</param>
         public Enemy(string name, int hp, int atk, int def) : base(name,hp,atk,def)
         {
 
