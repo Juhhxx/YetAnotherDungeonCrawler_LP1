83fbb46ca0dd424f587b1fecf2e5e4f02550c66c
notCroptu <martinsmariana1972@gmail.com>
2024-05-26 15:46:03 +0100

    Add documentation to Item class

--- DungeonCrawler/Item.cs
+++ DungeonCrawler/Item.cs
@@ -5,12 +5,24 @@ using System.Threading.Tasks;
 
 namespace DungeonCrawler
 {
+    /// <summary>
+    /// Class that contains the Item traits
+    /// </summary>
     public class Item
     {
+        // Initialize Item Name.
         public string Name { get; }
+        // Initialize Type of Buff.
         public BuffType Type { get; }
+        //Initialize value for buff.
         public int BuffValue { get; }
 
+        /// <summary>
+        /// Constructor for Item.
+        /// </summary>
+        /// <param name="name">Name the item will be identified by.</param>
+        /// <param name="type">Type of buff the Item will inflict on the Player.</param>
+        /// <param name="buff">Value of the buff the Item will inflict on the Player.</param>
         public Item(string name, BuffType type, int buff)
         {
             Name = name;
