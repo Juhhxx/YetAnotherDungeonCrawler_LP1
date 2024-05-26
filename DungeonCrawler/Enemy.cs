026fbf9c497a405ba36e5b7e4b641aa0216677b3
Julia Costa <juliacosta232005@gmail.com>
2024-05-26 20:52:43 +0100

    Add Enemy description

--- DungeonCrawler/Enemy.cs
+++ DungeonCrawler/Enemy.cs
@@ -8,6 +8,8 @@ namespace DungeonCrawler
     // Enemy class identifies all the non Player class characters
     public class Enemy : Character
     {
+        // Initialize Enemy description
+        public string Description { get; }
         /// <summary>
         /// Enemy constructor
         /// </summary>
@@ -15,9 +17,10 @@ namespace DungeonCrawler
         /// <param name="hp">The HP to be used by Enemy which is returned to Character base.</param>
         /// <param name="atk">The attack power to be used by Enemy which is returned to Character base.</param>
         /// <param name="def">The defense to be used by Enemy which is returned to Character base.</param>
-        public Enemy(string name, int hp, int atk, int def) : base(name,hp,atk,def)
+        public Enemy(string name, string desc, int hp, int atk, int def) : base(name,hp,atk,def)
         {
-
+            // Set Enemy Description
+            Description = desc;
         }
     }
 }
